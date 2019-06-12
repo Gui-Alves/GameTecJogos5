153b42dffc4ddf1d812af11787226420012d3916
Guilherme <guilherme.evangelista7@outlook.com.br>
2019-06-12 17:21:25 -0300

    Add project files.

---
+++ GameTecJogos5/Components/Jogador.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTecJogos5.Components
+{
+    public class Jogador
+    {
+        public float Velocidade { get; set; }
+        public Vector2 Posicao { get; set; } //inicio
+        public Vector2 Direcao { get; set; } //termino
+        public Texture2D Textura { get; set; }
+        public Rectangle Frame { get; set; }
+
+        public Jogador()
+        {
+        }
+
+        public Jogador(ContentManager content, Vector2 posicao, Vector2 direcao, float velocidade = 0)
+        {
+            Textura = content.Load<Texture2D>("jogador");
+            Posicao = posicao;
+            Velocidade = velocidade;
+            Direcao = direcao;
+        }
+
+        public void Movimentar(GameTime gameTime)
+        {
+            //Tempo real em segundos (deltatime)
+            float tempoExecucao = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //Calcular a movimentacao constante
+            Posicao += Velocidade * Direcao * tempoExecucao;
+        }
+
+        public void Desenhar(SpriteBatch spriteBatch)
+        {
+            Frame = new Rectangle(Convert.ToInt16(Posicao.X), Convert.ToInt16(Posicao.Y), Textura.Width, Textura.Height);
+            spriteBatch.Draw(Textura, Frame, Color.White);
+        }
+
+    }
+}
---
+++ GameTecJogos5/Game1.cs
@@ -0,0 +1,86 @@
+﻿using GameTecJogos5.Components;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+namespace GameTecJogos5
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Bola bola;
+        Jogador jogador1, jogador2;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            bola = new Bola(Content, new Vector2(100, 100), new Vector2(0, -1), 10);
+            jogador1 = new Jogador(Content, new Vector2(0, graphics.PreferredBackBufferHeight/2), Vector2.Zero, 100);
+            jogador2 = new Jogador(Content, new Vector2(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight / 2), Vector2.Zero, 100);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+            bola.Movimentar(gameTime);
+            var kys = Keyboard.GetState();
+            if (kys.IsKeyDown(Keys.Up))
+                jogador1.Direcao = new Vector2(0, -1);
+            if (kys.IsKeyDown(Keys.Down))
+                jogador1.Direcao = new Vector2(0, +1);
+            if (kys.IsKeyDown(Keys.Right))
+                jogador1.Direcao = new Vector2(+1, 0);
+            if (kys.IsKeyDown(Keys.Left))
+                jogador1.Direcao = new Vector2(-1, 0);
+            jogador1.Movimentar(gameTime);
+
+            if (kys.IsKeyDown(Keys.W))
+                jogador2.Direcao = new Vector2(0, -1);
+            if (kys.IsKeyDown(Keys.S))
+                jogador2.Direcao = new Vector2(0, +1);
+            if (kys.IsKeyDown(Keys.D))
+                jogador2.Direcao = new Vector2(+1, 0);
+            if (kys.IsKeyDown(Keys.A))
+                jogador2.Direcao = new Vector2(-1, 0);
+            jogador2.Movimentar(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            bola.Desenhar(spriteBatch);
+            jogador1.Desenhar(spriteBatch);
+            jogador2.Desenhar(spriteBatch);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
